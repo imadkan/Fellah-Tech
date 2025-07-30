f789e3c2fe8d15fe0fec42107f8cbdbfa540d1fd
imadkan <imadkan.busines@gmail.com>
2025-07-30 18:29:24 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>مرحبا بك في مشروع Fellah-Tech 🌾</h1>
+      <p>هذا مشروع Next.js جاهز للنشر على Vercel</p>
+    </div>
+  );
+}
